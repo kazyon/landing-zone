a8cd43cf9f9d246f6b7baaf6c55819552911cffe
kazyon <bacatea@gmail.com>
2020-10-22 00:03:20 +0300

    adding links to document

--- lighthouse.ps1
+++ lighthouse.ps1
@@ -39,3 +39,15 @@ https://www.altaro.com/msp-dojo/onboard-azure-lighthouse/
 https://www.altaro.com/msp-dojo/microsoft-365-lighthouse-msp/
 
 https://github.com/Azure/Azure-Lighthouse-samples
+
+# App registration section 
+
+https://moimhossain.com/2020/04/03/azure-ad-app-via-arm-template-deployment-scripts/
+
+https://stackoverflow.com/questions/56565980/how-to-use-arm-templates-to-deploy-a-roleassignment-for-an-app-registration-serv
+
+https://feedback.azure.com/forums/169401-azure-active-directory/suggestions/39142471-add-aad-app-registration-support-to-arm-template
+
+https://microsoft.github.io/botframework-solutions/virtual-assistant/tutorials/deploy-assistant/web/3-deploy-arm-template/
+
+https://dev.to/omiossec/arm-template-what-s-new-for-2020-4kli
